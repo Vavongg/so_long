07b36a360efb0f5c1c860c35a718b94ecdae9caa
root <root@PC-Alexis.>
2025-05-05 12:55:28 +0200

    update so_long.h

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ainthana <ainthana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 16:34:45 by ainthana          #+#    #+#             */
-/*   Updated: 2025/05/03 15:55:55 by ainthana         ###   ########.fr       */
+/*   Updated: 2025/05/05 12:54:10 by ainthana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,82 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-# define ADVANCE 119
-# define BACK 115
-# define RIGHT 100
-# define LEFT 97
-# define ESC 65307
+/* ************************************************************************** */
+/*                                Définitions des touches                     */
+/* ************************************************************************** */
+# define ADVANCE 119  // Touche W pour avancer
+# define BACK 115     // Touche S pour reculer
+# define RIGHT 100    // Touche D pour aller à droite
+# define LEFT 97      // Touche A pour aller à gauche
+# define ESC 65307    // Touche ESC pour quitter
 # define RED_BUTTON 79
 # define CLOSERED 33
 
+/* ************************************************************************** */
+/*                          Structure                                         */
+/* ************************************************************************** */
 typedef struct s_long
 {
+	// Carte du jeu (tableau 2D)
 	char			**map;
+
+	// Dimensions de la carte
 	int				width;
 	int				height;
+
+	// Position actuelle et précédente du joueur
 	int				x;
 	int				y;
 	int				oldx;
+
+	// Compteurs pour les objets trouvés
 	int				collectibles_found;
 	int				exits_found;
+
+	// Position du joueur sur la carte
 	int				player_x;
 	int				player_y;
+
+	// Dernière position du joueur
 	int				lastplayer_x;
 	int				lastplayer_y;
+
+	// Compteurs de collectibles
 	int				collectible;
 	int				collectible_total;
+
+	// Compteur de déplacements
 	int				move;
+
+	// Flags pour initialisation des éléments
 	int				playerset;
 	int				exitset;
 	int				collectibleset;
 
+	// Pointeur pour l'image pixel
 	char			*pxl;
+	// Paramètres de texture pour l'image
 	int				bpp;
 	int				s_line;
+
+	// Dimensions des cases de la carte
 	double			casetotal;
 	double			casein;
+
+	// Endianness
 	int				ed;
+
+	// Pointeurs pour l'affichage avec mlx
 	void			*mlx_ptr;
 	void			*mlx_win;
 	void			*img;
+
+	// Tableau pour gérer les entrées clavier
 	int				keyboard[70000];
+	// Indicateur pour libérer la carte
 	int				maptofree;
 
+	// Paramètres pour gérer les textures
 	int				bpp_text[10];
 	int				sline_text[10];
 	int				ed_text[10];
@@ -67,13 +102,19 @@ typedef struct s_long
 	int				widthtext[10];
 	int				heighttext[10];
 	int				texture;
+
+	// Couleur pour l'affichage
 	unsigned int	color;
 }					t_long;
 
-//main
+/* ************************************************************************** */
+/*                             Main                                           */
+/* ************************************************************************** */
 int		main(int ac, char **av);
 
-//parsing
+/* ************************************************************************** */
+/*                               Parsing                                      */
+/* ************************************************************************** */
 int		check_file(char *str, t_long *game);
 int		floodandfill(t_long *game, int mapy, int mapx);
 int		check_walls(int mapx, int mapy, t_long *game);
@@ -86,10 +127,10 @@ int		checkifgood(char c);
 int		variables_init(t_long *game);
 int		get_position(t_long *game, char *filename);
 int		parsing(t_long *game, char *av);
-void	floodandfill2(t_long *game, int mapy, int mapx);
-char	replace_char(char c);
 
-//utils
+/* ************************************************************************** */
+/*                               Utils                                        */
+/* ************************************************************************** */
 void	ft_putchar(char c);
 char	*ft_strcpy(char *dest, char *src);
 void	ft_putnbr(int nb);
@@ -102,7 +143,9 @@ int		error_msg(t_long *game, char *str);
 int		get_next_line(int fd, char **line);
 int		is_whitespace(char *str);
 
-//render
+/* ************************************************************************** */
+/*                            Render                                          */
+/* ************************************************************************** */
 int		display(t_long *game);
 int		go_hooking(t_long *game);
 int		key_loop(t_long *game);
